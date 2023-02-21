4eec9aa4b471a3c86c209b8b5b388b61722bad21
Daniel <danielpsev@gmail.com>
2023-02-21 22:33:16 +0200

    fetch

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,47 +1,49 @@
-// import React from "react";
+import React, { useState, useEffect } from 'react';
+import {v4 as uuidv4} from "uuid";
+
 import PostImage from "../PostImage/PostImage";
 import PostContent from "../PostContent/PostContent";
-import {v4 as uuidv4} from "uuid";
-import React, { useState, useEffect } from 'react';
+
+
 
 export default function Posts(props){
-    var list;
+
+    const [posts, setPosts] = useState([]);
+
     useEffect(() => {
+        let unsub = false;
         const fetchData = async () => {
           const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
           const newData = await response.json();
-          list = test(newData);
+         
+         if(!unsub){
+            console.log(newData)
+            setPosts(newData);
+         }
         };
         fetchData();
-      });
+
+        return () => {
+            unsub = true;
+            console.log("unsub true");
+        }
+
+      }, []);
+      
       
 
-      function test(data){
-        console.log('testing');
-        let inf = data.map((post) => {
-            return (
-                <div className='post' key={uuidv4()}>
-                    <PostImage url='img/header.jpg'/>
-                    <PostContent title={post.title} desc={post.body}/>
-                </div>
-            )
-         });
-         return inf;
-      }
-
-
-    let posts = [
-        {
-            title: "TITLE 1",
-            desc: "1. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-            img: "img/header.jpg"
-        },
-        {
-            title: "TITLE 2",
-            desc: "2. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
-            img: "https://th.bing.com/th/id/OIP.yRlxAuokAxQWJNUnVJANhQHaF7?pid=ImgDet&rs=1"
-        }
-    ];
+    // let posts = [
+    //     {
+    //         title: "TITLE 1",
+    //         desc: "1. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    //         img: "img/header.jpg"
+    //     },
+    //     {
+    //         title: "TITLE 2",
+    //         desc: "2. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+    //         img: "https://th.bing.com/th/id/OIP.yRlxAuokAxQWJNUnVJANhQHaF7?pid=ImgDet&rs=1"
+    //     }
+    // ];
 
     // let list = posts.map((post) => {
     //     return (
@@ -51,6 +53,16 @@ export default function Posts(props){
     //         </div>
     //     )
     // });
+
+     let list = posts.map((post) => {
+        return (
+            <div className='post' key={uuidv4()}>
+                <PostImage url='https://th.bing.com/th/id/OIP.yRlxAuokAxQWJNUnVJANhQHaF7?pid=ImgDet&rs=1'/>
+                <PostContent title={post.title} desc={post.body}/>
+            </div>
+        )
+    })
+
 
     return(
         <div className='posts-container'>
