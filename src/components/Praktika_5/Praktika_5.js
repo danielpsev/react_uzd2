8a266541267b626ff7049dbda6b509c75e6dca2b
Daniel <danielpsev@gmail.com>
2023-02-27 21:16:26 +0200

    tasks table

--- src/components/Praktika_5/Praktika_5.js
+++ src/components/Praktika_5/Praktika_5.js
@@ -6,15 +6,69 @@ import Task from "./Task/Task";
 export default function Praktika_5() {
   let tasks = [
     {
-      task: "Task 1 ",
+      task: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
       status: false,
     },
     {
-      task: "Task 2 ",
+      task: "Contrary to popular belief, Lorem Ipsum is not simply random text. ",
       status: false,
     },
     {
-      task: "Task 3 ",
+      task: "There are many variations of passages of Lorem Ipsum available,",
+      status: false,
+    },
+    {
+      task: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, ",
+      status: false,
+    },
+    {
+      task: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      status: false,
+    },
+  ];
+
+  let tasks_list = tasks.map((el) => {
+    return <Task key={uuidv4()} text={el.task} status={el.status}></Task>;
+    
+  });
+
+  return( 
+  <div className="d-flex flex-column">{tasks_list}
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Task</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {tasks_list}
+  </tbody>
+</table>
+
+  </div>);
+}
+
+
+/*
+import React, { useState } from "react";
+import "./Praktika_5.css";
+import { v4 as uuidv4 } from "uuid";
+// import LessText from "./LessText/LessText";
+import Task from "./Task/Task";
+export default function Praktika_5() {
+  let tasks = [
+    {
+      task: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+      status: false,
+    },
+    {
+      task: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,",
+      status: false,
+    },
+    {
+      task: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.",
       status: false,
     },
     {
@@ -29,7 +83,37 @@ export default function Praktika_5() {
 
   let tasks_list = tasks.map((el) => {
     return <Task key={uuidv4()} text={el.task} status={el.status}></Task>;
+    
   });
 
-  return <div className="d-flex flex-column">{tasks_list}</div>;
+  return( 
+  <div className="d-flex flex-column">{tasks_list}
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Otto</td>
+      <td>      
+      <button
+        className="btn btn-primary"
+      >
+        Check
+      </button>
+        
+        <button
+        className="mx-2 btn btn-danger" >
+        Delete
+      </button></td>
+    </tr>
+  </tbody>
+</table>
+
+  </div>);
 }
+
+*/
--- src/components/Praktika_5/Task/Task.js
+++ src/components/Praktika_5/Task/Task.js
@@ -1,6 +1,53 @@
 import React, { useState } from "react";
 import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
 export default function Task(props) {
+
+  const { text, status } = props;
+  const [task, setTask] = useState({ task: text, status: status });
+
+
+  const completeTask = () => {
+    setTask({ ...task, status: true })
+  }
+  const removeTask = (e) => {
+   //e.target.parentNode.remove();
+   setTask({});
+   e.target.parentNode.parentNode.remove();
+  };
+
+  let div_status_class = task.status ? " table-success " : " table-warning ";
+  let del_btn_status_class = task.status ? " btn-outline-success disabled " : "btn-outline-primary ";
+
+ // let icon_status_class = task.status ? " table-success " : " table-warning ";
+  return (
+    <tr className={"p2 mt-3 bg-opacity-10 "}>
+      <td className={"text-dark" + div_status_class}><b style={{fontSize: 25}}>{task.status ? <FaRegCheckCircle /> : <FaRegCircle />}</b>
+      </td>
+      <td>{task.task}</td>
+      <td>
+        <button
+        className={"btn "+ del_btn_status_class}
+        onClick={() => completeTask()}
+      >
+        {task.status ? "Done" : "Check"}
+      </button>
+      <button
+        className="btn btn-outline-danger mx-2"
+        onClick={(event) => removeTask(event)}
+      >
+        Delete
+      </button>
+      </td>
+    </tr>
+  );
+}
+
+
+
+/*
+import React, { useState } from "react";
+import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
+export default function Task(props) {
   const { text, status } = props;
   const [task, setTask] = useState({ task: text, status: status });
 
@@ -28,3 +75,5 @@ export default function Task(props) {
     </div>
   );
 }
+
+*/
