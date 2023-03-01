ea5881f46688c56b02b3f8921426bda3fb50a01e
danielpsev <danielpsev@gmail.com>
2023-03-01 09:18:35 +0200

    .

--- src/components/Praktika_5/Task/Task.js
+++ src/components/Praktika_5/Task/Task.js
@@ -3,21 +3,11 @@ import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
 export default function Task(props) {
   const { id, text, status, changeStatus, deleteTask } = props;
 
-  // const completeTask = () => {
-  //   setTask({ ...task, status: true });
-  // };
-  // const removeTask = (e) => {
-  //   //e.target.parentNode.remove();
-  //   setTask({});
-  //   e.target.parentNode.parentNode.remove();
-  // };
-
   let div_status_class = status ? " table-success " : " table-warning ";
   let del_btn_status_class = status
     ? " btn-outline-success disabled "
     : "btn-outline-primary ";
 
-  // let icon_status_class = task.status ? " table-success " : " table-warning ";
   return (
     <tr className={"p2 mt-3 bg-opacity-10 "}>
       <td className={"text-dark" + div_status_class}>
@@ -43,37 +33,3 @@ export default function Task(props) {
     </tr>
   );
 }
-
-/*
-import React, { useState } from "react";
-import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
-export default function Task(props) {
-  const { text, status } = props;
-  const [task, setTask] = useState({ task: text, status: status });
-
-  const removeTask = (e) => {
-   e.target.parentNode.remove();
-   setTask({});
-  };
-  let div_status_class = task.status ? " bg-success " : " bg-warning ";
-  return (
-    <div className={"p2 mt-3 bg-opacity-25 " + div_status_class}>
-      {task.status ? <FaRegCheckCircle /> : <FaRegCircle />}
-      {task.task}
-      <button
-        className="btn btn-primary"
-        onClick={() => setTask({ ...task, status: true })}
-      >
-        {task.status ? "Done" : "Check"}
-      </button>
-      <button
-        className="mx-2 btn btn-danger"
-        onClick={(event) => removeTask(event)}
-      >
-        Delete
-      </button>
-    </div>
-  );
-}
-
-*/
